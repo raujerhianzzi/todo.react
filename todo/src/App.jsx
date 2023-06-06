666250b4ccd4fb5a298c3d07490b5c977859a593
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2023-06-05 21:50:54 -0300

    Corpo do app
    criamos as divs das categorias

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -2,7 +2,42 @@ import { useState } from 'react'
 
 
 function App() {
-  return <div>Olá react</div>;
+  const [todos, settodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no Sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a Academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar react",
+      category: "Estudos",
+      isCompleted: false,
+    },    
+  ])
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo) => (
+        <div className="todo">
+          <div className='content'>
+            <p>{todo.text}</p>
+            <p className='category'>
+              {todo.category}
+            </p>
+          </div>
+        </div>
+      ))}
+
+    </div>
+  </div>;
 }
 
 export default App
